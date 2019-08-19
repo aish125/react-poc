96b537e6da47ba3959940f96f015295df75bf98f
Aishwarya <32268805+aish125@users.noreply.github.com>
2019-08-19 12:56:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
+import {connect} from 'react-redux'
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.getName = this.getName.bind(this);
     this.getLastName = this.getLastName.bind(this);
@@ -13,31 +14,44 @@ class App extends Component {
   }
   getName(e){
     this.setState({firstName: e.target.value})
-
   }
-  getLastName(e){
-    this.setState({lastName:e.target.value})
-
+  getLastName(e) {
+  this.setState({lastName: e.target.value})
   }
-  showName(){
-    
-  }
-  render(){
+  render() {
+    console.log("render");
     return (
-      <div className="App">
-        <div className="form">
-         FirstName <input type="text" placeholder="FirstName" value={this.state.firstName} onChange = {this.getName}></input>
-         <br/>
-         LastName <input type="text" value={this.state.lastName} placeholder="LastName" onChange = {this.getLastName}></input>
-         <button onClick = {this.showName}>Show Name</button>
-         <h1>{this.state.firstName} </h1>
-         <h1>{this.state.lastName}</h1>
+        <div className="App">
+          <div className="form">
+            FirstName <input type="text" placeholder="FirstName" value={this.state.firstName} onChange = {this.getName}></input>
+            <br/>
+            LastName <input type="text" value={this.state.lastName} placeholder="LastName" onChange = {this.getLastName}></input>
+            <br />
+            <button onClick = {this.showName}>Show Name</button>
+            <h1>{this.state.firstName} </h1>
+            <h1>{this.state.lastName}</h1>
+          </div>
+          <div className="Age-label">
+            your age: <span>{this.props.age}</span>
+          </div>
+          <button onClick={this.props.onAgeUp}>Age UP</button>
         </div>
-      
-      </div>
     );
-  }
-  }
- 
+    }
+}
 
-export default App;
+const mapStateToProps= state =>{
+  console.log("mapStateToProps");
+  return {
+    age: state.age
+  };
+};
+
+const mapDispatchToProps = dispatch =>{
+  console.log("mapDispatchToProps");
+  return {
+    onAgeUp: ()=> dispatch({type: 'Add'})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
